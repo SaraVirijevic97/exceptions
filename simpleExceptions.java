8702a984edeb4e57a5921799374f0bdbe86b530c
SaraVirijevic97 <110235510+SaraVirijevic97@users.noreply.github.com>
2022-08-09 14:13:34 +0200

    Update simpleExceptions.java

--- simpleExceptions.java
+++ simpleExceptions.java
@@ -1,23 +1,28 @@
 package exceptions;
 
+import java.util.Scanner;
+
 public class simpleExceptions {
 
     public static void main(String[] args) {
-        
+
         try {
             String[] number = {"one", "two", "three", "four", "five"};
-            System.out.println(number[7]);
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter the index");
+            int n = keyboard.nextInt();
+            System.out.println(number[n]);
         }
         catch (NullPointerException e){
             System.out.println("This array is null!");
         }
         catch (ArrayIndexOutOfBoundsException e)
         {
-            System.out.println("Indes is out of bounds!");
+            System.out.println("Index is out of bounds!");
         }
         catch (Exception e) {
             System.out.println("This is an exception.");
         }
-        
+
     }
 }
